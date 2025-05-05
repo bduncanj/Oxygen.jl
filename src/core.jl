363c323363c1de8656012e11fc4be1e86eb3c19b
Duncan Brown <code@unclouded.com>
2025-05-05 20:57:11 +0000

    Drop needless response extraction

--- src/core.jl
+++ src/core.jl
@@ -386,13 +386,7 @@ and bypassing any globally defined middleware
 """
 function internalrequest(ctx::ServerContext, req::HTTP.Request; middleware::Vector=[], metrics::Bool=false, serialize::Bool=true, catch_errors=true)::HTTP.Response
     req.context[:ip] = "INTERNAL" # label internal requests
-    res = req |> setupmiddleware(ctx; middleware, metrics, serialize, catch_errors)
-    # When running inside test suite this can be a `ResponseWrapper` instead of expected `HTTP.Response`
-    if res isa ResponseWrapper
-        return res.response
-    else
-        return res
-    end
+    return req |> setupmiddleware(ctx; middleware, metrics, serialize, catch_errors)
 end
 
 
