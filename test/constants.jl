c6e67e09e0ae5311bc331c673413c28cdadb53ae
Duncan Brown <code@unclouded.com>
2025-05-06 16:25:39 +0000

    Test: Add MIME_MAP to constants

--- test/constants.jl
+++ test/constants.jl
@@ -1,9 +1,19 @@
 module Constants
 
-export HOST, PORT, localhost
+export HOST, PORT, localhost, MIME_MAP
 
 const HOST = "127.0.0.1"
 const PORT = 6060 # 8080 port is frequently used
 const localhost = "http://$HOST:$PORT"
+
+const MIME_MAP::Dict{Symbol,String} = Dict(
+    :Html => "text/html",
+    :Text => "text/plain",
+    :Json => "application/json",
+    :Xml => "application/xml",
+    :Js => "application/javascript",
+    :Css => "text/css",
+    :Binary => "application/octet-stream"
+)
 
 end
